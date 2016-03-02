eb3b0b5f9d804e19a6c04f04dd70280f2bac73f9
Sadusko <Sadusko@users.noreply.github.com>
2016-03-02 09:36:17 +0100

    Update
    Updated.

--- index.php
+++ index.php
@@ -19,12 +19,15 @@
           echo ("No arguments were given!");
         break;
       }
-echo '<center><h1><div style="width:500px;margin:auto;">';
+
+
+      echo '<center><h1><div style="width:500px;margin:auto;">';
       echo ("Total: " . $total);
       echo ("<br><br>First number given: " . $number1 . "<br>Type of math: " . $type . "<br>Second number given: " . $number2);
-
       echo ("<br><br><br>" . $number1 . " " . $type . " " . $number2 . " = " . $total);
-echo '</div></h1></center>';
-  }
-  calculate ("+", 5, 250);
-  ?>
+      echo '</div></h1></center>';
+
+
+    }
+      calculate ("+", 5, 250);
+?>
